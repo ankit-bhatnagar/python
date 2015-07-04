fbcead6096495bd26c39645f1f22010b08e28236
Ankit Bhatnagar <ankit.bhatnagar4@gmail.com>
2015-07-04 01:34:43 -0700

    Finished the functions

--- list_functions.py
+++ list_functions.py
@@ -1,11 +1,24 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
+#Hacker Rank
+# Performing various functions on a List
 L=[]
 def match(c):
-    if c[0]=='insert':
-        L.append(c[1])
-        L.append(c[2])
-    elif c[0]=='print'    
-    
+	if c[0]=='insert':
+		L.insert(int(c[1]),int(c[2]))
+	elif c[0]=='print':
+		print L
+	elif c[0]=='remove':
+		L.remove(int(c[1]))
+	elif c[0]=='append':
+		L.append(int(c[1]))
+	elif c[0]=='sort':
+		L.sort()
+	elif c[0]=='reverse':
+		L.reverse()
+	elif c[0]=='pop':
+		L.pop()
+	else:
+		return
+	
 N=int(raw_input())
 for i in range(N):
     command=raw_input()
